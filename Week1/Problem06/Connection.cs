d327e1596cbcf626e1c12254a0cb3af65f211741
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-03-26 02:40:01 +0300

    I finished the 06th Problem

---
+++ Week1/Problem06/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem06
+{
+    public class Connection
+    {
+        public int id { get; private set; }
+        protected Connection(int id)
+        {
+            this.id = id;
+        }
+    }
+}
