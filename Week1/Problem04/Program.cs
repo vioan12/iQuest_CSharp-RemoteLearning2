7be7389213ce3bd191ab64824cb7216ae9367632
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-03-25 16:55:12 +0300

    I finished the 04th Problem

--- Week1/Problem04/Program.cs
+++ Week1/Problem04/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace Problem04
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static PrimeNumber primeNumber;
+        public static int Main(string[] args)
         {
+            int maximumNumber;
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a numeric argument.");
+                return 1;
+            }
+            bool test = int.TryParse(args[0], out maximumNumber);
+            if (test == false)
+            {
+                System.Console.WriteLine("Please enter a correct numeric argument.");
+                return 1;
+            }
+            primeNumber = new PrimeNumber();
+            System.Console.Write("1");
+            for (int i = 2; i <= maximumNumber; i++)
+            {
+                if (primeNumber.IsPrime(i))
+                {
+                    System.Console.Write(", " + i + "-PRIME");
+                }
+                else
+                {
+                    System.Console.Write(", " + i);
+                }
+            }
+            return 0;
         }
     }
 }
