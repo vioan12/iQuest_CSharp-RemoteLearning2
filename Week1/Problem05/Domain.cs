ee35965d612be4963dd0368a720592115168bc5f
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-03-26 00:19:05 +0300

    I finished the 05th Problem

---
+++ Week1/Problem05/Domain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem05
+{
+    public class Domain
+    {
+        public string name { get; private set; }
+        public string hosts { get; private set; }
+        public string ownerDetails { get; private set; }
+        public Domain(string name, string hosts, string ownerDetails)
+        {
+            this.name = name;
+            this.hosts = hosts;
+            this.ownerDetails = ownerDetails;
+        }
+        public bool IsEquals(Domain domain)
+        {
+            if(name == domain.name)
+            {
+                if(hosts == domain.hosts)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Week1/Problem05/Reseller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem05
+{
+    public class Reseller : Subject, IObserver 
+    {
+        public void Update(Domain domain)
+        {
+            Notify(domain);
+        }
+    }
+}
