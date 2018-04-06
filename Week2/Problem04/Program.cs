b30c27dc90ec75e0e5da4b75c650671bede55ea9
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-04-06 18:35:09 +0300

    I'm working on the 02nd Problem

--- Week2/Problem04/Program.cs
+++ Week2/Problem04/Program.cs
@@ -8,13 +8,36 @@ namespace Problem04
 {
     public class Program
     {
-        private static ReadFiles readFile;
         private static CustomersCollection customersCollection;
         public static void Main(string[] args)
         {
             customersCollection = new CustomersCollection();
-            readFile = new ReadFiles(Constants.FileNameCustomersDetails, Constants.FileNameReservationsDetails, customersCollection);
-            readFile.Read();
+            try
+            {
+                List<string> customersFileLines, reservationsFileLines;
+                List<Customer> customersList;
+                List<UsernameReservation> usernameReservationsList;
+                Converter converter = new Converter();
+                ReadFile readFile;
+                readFile = new ReadFile(Constants.FileNameCustomersDetails);
+                customersFileLines = readFile.Read();
+                readFile = new ReadFile(Constants.FileNameReservationsDetails);
+                reservationsFileLines = readFile.Read();
+                customersList = converter.ConvertCustomersFile(customersFileLines);
+                usernameReservationsList = converter.ConvertReservationsFile(reservationsFileLines);
+                foreach (Customer customer in customersList)
+                {
+                    customersCollection.AddCustomer(customer);
+                }
+                foreach (UsernameReservation usernameAndReservation in usernameReservationsList)
+                {
+                    customersCollection.FindByUsername(usernameAndReservation.Username).AddReservation(usernameAndReservation.Reservation);
+                }
+            }
+            catch (Exception exception)
+            {
+                System.Console.WriteLine(exception.Message);
+            }
         }
     }
 }
---
+++ Week2/Problem04/UsernameReservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem04
+{
+    public class UsernameReservation
+    {
+        public string Username { get; set; }
+        public Reservation Reservation { get; set; }
+    }
+}
