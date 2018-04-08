8b27d4cdf5eb0205db922ca147132c24154b8715
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-04-08 17:56:32 +0300

    I solved the comments from 03rd Problem - Week2

--- Week2/Problem03/Program.cs
+++ Week2/Problem03/Program.cs
@@ -11,8 +11,10 @@ namespace Problem03
         private static Compression compression;
         public static void Main(string[] args)
         {
+            List<FileLength> inputOutputFiles;
             compression = new Compression();
-            compression.Compress(Constants.SourceFileName);
+            inputOutputFiles = compression.Compress(Constants.SourceFileName);
+            Console.WriteLine("Compressed " + inputOutputFiles.ElementAt(0).Name + " from " + inputOutputFiles.ElementAt(0).Length.ToString() + "bytes to " + inputOutputFiles.ElementAt(1).Length.ToString() + "bytes with " + ((double)100 * ((double)inputOutputFiles.ElementAt(0).Length - (double)inputOutputFiles.ElementAt(1).Length) / (double)inputOutputFiles.ElementAt(0).Length) + "% ratio");
         }
     }
 }
