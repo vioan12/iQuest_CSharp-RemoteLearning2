a45e6d388e2e36e10bb8c38435ac4a9ee3d2f6c7
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-04-13 17:26:31 +0300

    I moved the Problem02 outside of Week2

--- ProblemCustomerReservation/Converter.cs
+++ ProblemCustomerReservation/Converter.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Problem04
+namespace ProblemCustomerReservation
 {
     public class Converter
     {
--- ProblemCustomerReservation/IRepository.cs
+++ ProblemCustomerReservation/IRepository.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Problem04
+namespace ProblemCustomerReservation
 {
     public interface IRepository<TEntity> where TEntity : class
     {
